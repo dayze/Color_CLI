ca65300f65c2b1eeb282750ff62974b57520e9ea
Maxime Laine <maxime.laine@dsinstruments.fr>
2016-05-03 10:48:28 +0200

    Add of JSON file

--- Color.php
+++ Color.php
@@ -2,18 +2,36 @@
 
 class Color
 {
-    private static $arrayColor = array("black" => "0;30", "blue" => "0;34", "green" => "0;32", "cyan" => "0;36",
-        "red" => "0;31", "purple" => "0;35", "brown" => "0;33", "grayLight" => "0;37",
-        "darkGray" => "1;30", "blueLight" => "1;34", "greenLight" => "1;32",
-        "cyanLight" => "1;36", "redLight" => "1;31", "purpleLight" => "1;35",
-        "yellow" => "1;33", "white" => "1;37");
-
-    public static function applyColor($string, $color){
-        return "\033[" .self::$arrayColor[$color] . "m" . $string . "\033[" . self::$arrayColor['white'] . "m";
+    private static function decodeColor()
+    {
+        try{
+            return json_decode(file_get_contents('color.json'), true);
+        }catch (Exception $e){
+            throw  new Exception($e->getMessage());
+        }
     }
 
-    public static function echoApplyColor($string, $color){
-         echo self::applyColor($string, $color);
+    public static function applyColor($string, $color)
+    {
+        try{
+            $colorArray = self::decodeColor();
+            if(array_key_exists($color, $colorArray)){
+                return "\033[" . $colorArray[$color] . "m" . $string . "\033[0m";
+            }else{
+                throw new Exception ("COLOR_NOT_FOUND");
+            }
+            
+        }catch (Exception $e){
+            throw new Exception($e->getMessage());
+        }
     }
 
+    public static function echoApplyColor($string, $color)
+    {
+        try{
+            echo self::applyColor($string, $color);
+        }catch (Exception $e){
+            throw new Exception($e->getMessage());
+        }
+    }
 }
--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # Color_CLI
 Change the output color of CLI PHP script
-You can put the class into your project for add some color to your php cli output.
+You can put the class into your project to add some colors to your php cli output.
 All methods are static so you don't need to instanciate the class.
+
+##Example
+This is all the colors provided by the Color_CLI Class
+![Alt text](http://195.154.75.137/storage/color_cli/color_screen.png "Optional title")
+
+You can add your own into the JSON file "color.json"
+![Alt text](http://195.154.75.137/storage/color_cli/color_json_screen.png "Optional title")
